743e55ae0cf8ce4753908b581154519ddac9f4fb
clebemachado <clebmachado.em@gmail.com>
2022-06-21 14:22:48 -0300

    Melhor ajuste da 7 segunda lista

--- src/Modulo_1/Lista_2/Ex_6.java
+++ src/Modulo_1/Lista_2/Ex_6.java
@@ -12,6 +12,7 @@ public class Ex_6 {
     public static void main(String[] args) {
         int[] valores = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Entre com um valor: ");
         int valorUser = scanner.nextInt();
 
--- src/Modulo_1/Lista_2/Ex_7.java
+++ src/Modulo_1/Lista_2/Ex_7.java
@@ -7,26 +7,24 @@ Leia uma matriz 4 x 4, conte e escreva quantos valores maiores que 10 ela possui
  */
 public class Ex_7 {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int[][] matrizNumeros = new int[4][4];
 
+        int contador = 0;
+        int valoresDigitados = 0;
+
         for(int i = 0; i < 4; i ++){
             for(int j = 0; j < 4; j ++){
-                matrizNumeros[i][j] = scanner.nextInt();
+                valoresDigitados = scanner.nextInt();
+                matrizNumeros[i][j] = valoresDigitados;
                 scanner.nextLine();
-            }
-        }
-
-        int contador = 0;
-        for (int[] matrizes:matrizNumeros) {
-            for(int valores: matrizes){
-                if(valores >= 10){
+                if(valoresDigitados >= 10){
                     contador += 1;
                 }
             }
         }
 
         System.out.println("Quantidade de valores maiores que 10: " + contador);
-
     }
 }
