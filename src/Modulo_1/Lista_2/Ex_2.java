45940c0fa83b1c5865f427df8e807e568b6607e7
clebemachado <clebmachado.em@gmail.com>
2022-06-21 13:44:31 -0300

    Ajustado 2 questão lista 2

--- src/Modulo_1/Lista_2/Ex_2.java
+++ src/Modulo_1/Lista_2/Ex_2.java
@@ -10,14 +10,13 @@ public class Ex_2 {
         int valorAleatorio = new Random().nextInt(1000);
 
         int valorUser;
-        System.out.println(valorAleatorio);
         while (true){
             System.out.print("Entre com um valor: ");
             valorUser = scanner.nextInt();
             scanner.nextLine();
 
             if(valorAleatorio > valorUser){
-                System.out.println("número a ser encontrado é maior do que você digitou.");
+                System.out.println("O número a ser encontrado é maior do que você digitou.");
             } else if(valorAleatorio < valorUser){
                 System.out.println("O número a ser encontrado é menor do que você digitou.");
             } else {
