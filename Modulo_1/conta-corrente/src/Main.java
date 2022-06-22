c70304eadece5ed8b6654783311d03771a008ea7
clebemachado <clebmachado.em@gmail.com>
2022-06-22 12:41:09 -0300

    Corrigido nomes das variáveis

--- Modulo_1/conta-corrente/src/Main.java
+++ Modulo_1/conta-corrente/src/Main.java
@@ -12,33 +12,33 @@ public class Main {
         endereco.numero = 202;
         endereco.pais = "Brasil";
 
-        Contato contato_1 = new Contato();
-        contato_1.descricao = "Número Janeide";
-        contato_1.telefone = "98985371232";
-        contato_1.tipo = 1;
+        Contato contato1 = new Contato();
+        contato1.descricao = "Número Janeide";
+        contato1.telefone = "98985371232";
+        contato1.tipo = 1;
 
-        Contato contato_2 = new Contato();
-        contato_2.descricao = "Contato escritório - Janeide";
-        contato_2.telefone = "98985375412";
-        contato_2.tipo = 2;
+        Contato contato2 = new Contato();
+        contato2.descricao = "Contato escritório - Janeide";
+        contato2.telefone = "98985375412";
+        contato2.tipo = 2;
 
-        Cliente cliente_1 = new Cliente();
-        cliente_1.nome = "Janeide";
-        cliente_1.cpf = "45141425235";
-        cliente_1.enderecos[0] = endereco;
-        cliente_1.contatos[0] = contato_1;
-        cliente_1.contatos[1] = contato_2;
+        Cliente cliente1 = new Cliente();
+        cliente1.nome = "Janeide";
+        cliente1.cpf = "45141425235";
+        cliente1.enderecos[0] = endereco;
+        cliente1.contatos[0] = contato1;
+        cliente1.contatos[1] = contato2;
 
-        ContaCorrente contaCorrente_1 = new ContaCorrente();
-        contaCorrente_1.chequeEspecial = 100.00;
-        contaCorrente_1.saldo = 100.00;
-        contaCorrente_1.agencia = 3333;
-        contaCorrente_1.numeroConta = "22.222-2";
-        contaCorrente_1.cliente = cliente_1;
+        ContaCorrente contaCorrente1 = new ContaCorrente();
+        contaCorrente1.chequeEspecial = 100.00;
+        contaCorrente1.saldo = 100.00;
+        contaCorrente1.agencia = 3333;
+        contaCorrente1.numeroConta = "22.222-2";
+        contaCorrente1.cliente = cliente1;
 
 
         // Cliente 2
-        Endereco endereco_2 = new Endereco();
+        Endereco endereco2 = new Endereco();
         endereco.tipo = 1;
         endereco.logradouro = "Rua Nice Lobão";
         endereco.cep = "65061000";
@@ -48,65 +48,65 @@ public class Main {
         endereco.numero = 101;
         endereco.pais = "Brasil";
 
-        Contato contato_3 = new Contato();
-        contato_1.descricao = "Número Maria";
-        contato_1.telefone = "47546616166";
-        contato_1.tipo = 1;
+        Contato contato3 = new Contato();
+        contato1.descricao = "Número Maria";
+        contato1.telefone = "47546616166";
+        contato1.tipo = 1;
 
-        Cliente cliente_2 = new Cliente();
-        cliente_2.nome = "Maria";
-        cliente_2.cpf = "46989737085";
-        cliente_2.enderecos[0] = endereco_2;
-        cliente_2.contatos[0] = contato_3;
+        Cliente cliente2 = new Cliente();
+        cliente2.nome = "Maria";
+        cliente2.cpf = "46989737085";
+        cliente2.enderecos[0] = endereco2;
+        cliente2.contatos[0] = contato3;
 
-        ContaCorrente contaCorrente_2 = new ContaCorrente();
-        contaCorrente_2.chequeEspecial = 100.00;
-        contaCorrente_2.saldo = 100.00;
-        contaCorrente_2.agencia = 2568;
-        contaCorrente_2.numeroConta = "30.000-x";
-        contaCorrente_2.cliente = cliente_2;
+        ContaCorrente contaCorrente2 = new ContaCorrente();
+        contaCorrente2.chequeEspecial = 100.00;
+        contaCorrente2.saldo = 100.00;
+        contaCorrente2.agencia = 2568;
+        contaCorrente2.numeroConta = "30.000-x";
+        contaCorrente2.cliente = cliente2;
 
         // Testes da transferencia
         System.out.println("SALDO DA MARIA ANTES DO SAQUE");
-        contaCorrente_2.imprimirContaCorrente();
+        contaCorrente2.imprimirContaCorrente();
 
-        boolean teste = contaCorrente_2.sacar(150.00);
+        boolean teste = contaCorrente2.sacar(150.00);
         imprimirTesteSaque(teste);
 
-        teste = contaCorrente_2.sacar(65.00);
+        teste = contaCorrente2.sacar(65.00);
         imprimirTesteSaque(teste);
-        contaCorrente_2.imprimirContaCorrente();
-        contaCorrente_2.depositar(35.00);
-        contaCorrente_2.imprimirContaCorrente();
+        contaCorrente2.imprimirContaCorrente();
+        contaCorrente2.depositar(35.00);
+        contaCorrente2.imprimirContaCorrente();
 
         System.out.println("SALDO DA JANIEDE ANTES DA TRANSFERÊNCIA PARA MARIA");
-        contaCorrente_1.imprimirContaCorrente();
-        teste = contaCorrente_1.transferir(contaCorrente_2, 85.00);
+        contaCorrente1.imprimirContaCorrente();
+        teste = contaCorrente1.transferir(contaCorrente2, 85.00);
         imprimirTransferencia(teste);
-        contaCorrente_2.imprimirContaCorrente();
+        contaCorrente2.imprimirContaCorrente();
 
-        contaCorrente_1.imprimirContaCorrente();
+        contaCorrente1.imprimirContaCorrente();
 
         // Testando deposito
         System.out.println("DEPOSITAR PARA CONTA DA JANIEDE");
-        teste = contaCorrente_1.depositar(-100.00);
+        teste = contaCorrente1.depositar(-100.00);
         imprimirDeposito(teste);
-        teste = contaCorrente_1.depositar(100.00);
+        teste = contaCorrente1.depositar(100.00);
         imprimirDeposito(teste);
-        contaCorrente_1.imprimirContaCorrente();
+        contaCorrente1.imprimirContaCorrente();
 
         // Testando sacar
         System.out.println("SACAR DA CONTA DA JANIEDE");
-        teste = contaCorrente_1.sacar(-100.00);
+        teste = contaCorrente1.sacar(-100.00);
         imprimirTesteSaque(teste);
-        teste = contaCorrente_1.sacar(100.00);
+        teste = contaCorrente1.sacar(100.00);
         imprimirTesteSaque(teste);
-        contaCorrente_1.imprimirContaCorrente();
+        contaCorrente1.imprimirContaCorrente();
 
         // IMPRIMINDO AS DUAS CONTAS
         System.out.println("MOSTRANDO AS CONTAS");
-        contaCorrente_1.imprimirContaCorrente();
-        contaCorrente_2.imprimirContaCorrente();
+        contaCorrente1.imprimirContaCorrente();
+        contaCorrente2.imprimirContaCorrente();
 
     }
 
