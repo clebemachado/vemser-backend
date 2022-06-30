69586a7f7f493d974424c5acf24b1751caaf18a4
clebemachado <clebmachado.em@gmail.com>
2022-06-30 11:55:40 -0300

    Adicionado melhorias

--- Modulo_2/homework2.sql
+++ Modulo_2/homework2.sql
@@ -3,31 +3,38 @@
 */
 
 -- Selecionar todos os países ordenados pro nome decrescente
-SELECT * FROM VEM_SER.PAIS p 
+SELECT * 
+FROM VEM_SER.PAIS p 
 ORDER BY p.NOME DESC;
 
 -- Selecionar logradouro e cep dos endereços. Porém, somente os logradouros que comecem com a letra 'a' (maiúsculo ou minúsculo);
-SELECT e.LOGRADOURO , e.CEP  FROM VEM_SER.ENDERECO e 
+SELECT e.LOGRADOURO , e.CEP 
+FROM VEM_SER.ENDERECO e 
 WHERE UPPER(e.LOGRADOURO ) LIKE  'A%';
 
 -- Selecionar todos os endereços que tenham cep com final ‘0’;
 SELECT * FROM VEM_SER.ENDERECO e 
-WHERE e.CEP LIKE '%0';
+WHERE TRIM(e.CEP) LIKE '%0';
 
 -- Selecionar todos os endereços que tenham números entre 1 e 100;
-SELECT * FROM VEM_SER.ENDERECO e 
+SELECT * 
+FROM VEM_SER.ENDERECO e 
 WHERE e.NUMERO BETWEEN 1 AND 100;
 
 -- Selecionar todos os endereços que comecem por “RUA” e ordenar pelo cep de forma decrescente ;
-SELECT * FROM VEM_SER.ENDERECO e 
+SELECT * 
+FROM VEM_SER.ENDERECO e 
 WHERE UPPER(e.LOGRADOURO) LIKE 'RUA%'
 ORDER BY e.CEP DESC ;
 
 -- Selecionar a quantidade de endereços cadastrados na tabela;
-SELECT COUNT(*) AS Qtd_Enderecos FROM VEM_SER.ENDERECO e ;
+SELECT COUNT(*) AS Qtd_Enderecos 
+FROM VEM_SER.ENDERECO e ;
 
 -- Selecionar a quantidade de endereços cadastrados agrupados pelo id da cidade;
-SELECT e.ID_CIDADE  AS ID_CIDADE, COUNT(*) AS Qtd_Enderecos FROM VEM_SER.ENDERECO e GROUP BY e.ID_CIDADE  ;
+SELECT e.ID_CIDADE AS ID_CIDADE, COUNT(*) AS Qtd_Enderecos 
+FROM VEM_SER.ENDERECO e 
+GROUP BY e.ID_CIDADE  ;
 
 
 /*
